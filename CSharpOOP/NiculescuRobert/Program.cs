a7cb920630e4eed0fab445f131ca744c9fefcd9d
RobertNiculescu <robert.niculescu00@e-uvt.ro>
2021-11-20 14:27:16 +0200

    madechanges

--- CSharpOOP/NiculescuRobert/Program.cs
+++ CSharpOOP/NiculescuRobert/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Security.Authentication.ExtendedProtection;
 
 namespace NiculescuRobert
 {
+    class Animal
+    {
+        
+        private string name;
+        public string color;
+        public string alias;
+        public string GetName()
+        {
+            return name;
+            
+        }
+        
+        public void SetName(string name)
+        {
+            this.name = name + '*';
+        }
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+        public Animal(string name, string color)
+        {
+            Console.WriteLine("constructor");
+            this.name = name;
+            this.color = color;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animal tiger = new Animal("Coll");
+            var anim1 = new Animal("Ryn", "black");
+            Console.WriteLine(anim1.color + anim1.name);
+            
+        
+        
+            Console.WriteLine("Destructor");
+            var newcat=new Animal{color ="ceva nume",alias ="grumpy cat"}
         }
+           
+        
     }
 }
