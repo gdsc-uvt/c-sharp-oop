ac71fb4a266efd39fe2879913991df4af63412f4
Stas <stasbrega@inbox.ru>
2021-11-20 14:19:38 +0200

    modify user

--- CSharpOOP/BregaStanislav/Program.cs
+++ CSharpOOP/BregaStanislav/Program.cs
@@ -2,11 +2,58 @@
 
 namespace BregaStanislav
 {
+
+    class Animal
+    {
+
+        private string name;
+        public string color;
+
+        public string GetName()
+        {
+
+            return name;
+
+        }
+
+        public void SetName(string newName)
+        {
+
+            name = newName + '*';
+
+        }
+        
+        public Animal(string name)
+        {
+            
+            this.name = name.ToUpper();
+            color = "black";
+            Console.WriteLine("new object");
+            
+        }
+
+        public Animal(string name, string color)
+        {
+            
+            this.name = name;
+            this.color = color;
+            Console.WriteLine("new object");
+            
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var cat = new Animal("Tom");
+            var dog = new Animal("Bobby", "White");
+            dog.SetName("NewName");
+            
+            Console.WriteLine(cat.GetName() + ' ' + cat.color);
+            Console.WriteLine(dog.GetName() + ' ' + dog.color);
+
         }
     }
 }
