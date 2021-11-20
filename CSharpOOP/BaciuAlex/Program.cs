9520ff187a0e063a55339d9a6b7921037e0b5a90
Baciu Alex <alex.baciu01@e-uvt.ro>
2021-11-20 14:17:44 +0200

    Update

--- CSharpOOP/BaciuAlex/Program.cs
+++ CSharpOOP/BaciuAlex/Program.cs
@@ -3,11 +3,64 @@ using System;
 
 namespace BaciuAlex
 {
+    class Animal
+    {
+        private string name;
+        public string color;
+
+        public Animal(string name)
+        {
+            this.name = name;
+            this.color = "black";
+        }
+        
+        public Animal(string name, string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return "Animal: " + name + " " + color;
+        }
+        
+        ~Animal()
+        {
+            Console.WriteLine("Destructor");
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
+        { 
+            Animal cat = new Animal("Tom");
+
+            Animal dog = new Animal("Sasha", "Gri");
+            
+            cat.setName("Pisica nume nou");
+            
+            Console.WriteLine(cat.GetName());
+            Console.WriteLine(cat.color);
+            
+            Console.WriteLine(dog.GetName());
+            Console.WriteLine(dog.color);
+            
+            Console.WriteLine(dog.ToString());
+
         }
     }
+    
+    
 }
