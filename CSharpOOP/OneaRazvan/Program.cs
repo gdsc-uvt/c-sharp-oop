38ba918b7494ec2724c529f97396ec2ea43d2aef
PeaceFlag <73926819+PeaceFlag@users.noreply.github.com>
2021-11-23 21:44:10 +0200

    Trebuie sa scriu comentariu mereu?

--- CSharpOOP/OneaRazvan/Program.cs
+++ CSharpOOP/OneaRazvan/Program.cs
@@ -6,8 +6,18 @@ public class AlbumFoto
     {
         PhotoAlbum myAlbum1 = new PhotoAlbum();
         Console.WriteLine(myAlbum1.GetNumberPages());
+        
+        PhotoAlbum myAlbum2 = new PhotoAlbum(32);
+        Console.WriteLine(myAlbum2.GetNumberPages());
 
         BigPhotoAlbum myBigPhotoAlbum1 = new BigPhotoAlbum();
+        Console.WriteLine(myBigPhotoAlbum1.GetNumberPages());
+        myBigPhotoAlbum1.AddPage();
+        Console.WriteLine(myBigPhotoAlbum1.GetNumberPages());
+        myBigPhotoAlbum1.AddPhoto();
+        myBigPhotoAlbum1.AddPhoto();
+        myBigPhotoAlbum1.AddPhoto();
+        myBigPhotoAlbum1.AddPhoto();
         Console.WriteLine(myBigPhotoAlbum1.GetNumberPages());
     }
 }
@@ -15,7 +25,7 @@ public class AlbumFoto
 public class PhotoAlbum
 {
     private int numPages;
- 
+    
     public PhotoAlbum()
     {
         numPages = 16;
@@ -33,11 +43,26 @@ public class PhotoAlbum
 }
  
 public class BigPhotoAlbum
-{
+{   private int photo = 1; 
     private int numPages;
     public BigPhotoAlbum()
     {
         numPages = 64;
+    }
+
+    public int AddPage()
+    {
+        return numPages++;
+    }
+
+    public int AddPhoto()
+    {
+        if (photo % 4 == 0)
+        {
+            AddPage();
+        }
+        else photo++;
+        return numPages;
     }
     public int GetNumberPages()
     {
